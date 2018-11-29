938a1f7c647745ca7e43818790ce3c1696ee623f
FabienCOTE <fabien.cote@gmail.com>
2018-11-29 10:40:19 +0100

    Fin

--- exercice_4.php
+++ exercice_4.php
@@ -6,11 +6,6 @@ if (isset($_GET['language']) && isset($_GET['server'])) {
 }
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
